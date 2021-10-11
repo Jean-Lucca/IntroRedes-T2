a58928e8ace52c3ab01f08f411efd37bdfe79ef2
Jean Lucca Uchaki <jean.lucca.uchaki@gmail.com>
2021-10-11 12:15:53 -0300

    bcc encoder updated

--- src/Bcc_encoder.java
+++ src/Bcc_encoder.java
@@ -1,3 +1,62 @@
 public class Bcc_encoder {
-    
+
+    public static void main(String[] args) {
+        encode("redes");
+    }
+
+    public static String encode( String str ) {
+        String res = "";
+        String[] binArray = new String[str.length()+1];
+        binArray[binArray.length-1] = "";
+
+        for( int i = 0; i < str.length(); i++ ) {
+            String aux = Integer.toBinaryString(str.charAt(i)-0);
+            binArray[i] = aux += countParity(aux);
+        }
+
+        for( int i = 0; i < 8; i++ ) {
+            String aux = "";
+            for( int j = 0; j < binArray.length-1; j++ ) {
+                aux += binArray[j].charAt(i);
+            }
+            binArray[binArray.length-1] += countParity(aux);
+        }
+
+        for( int j = 0; j < binArray.length; j++ ) {
+            res += Util.binaryToHex(binArray[j]);
+        }
+
+        return res;
+    }
+
+    public static String decode(String hex) {
+        String res = "";
+        String bin = Util.hexToBinary(hex);
+        String[] binArray = new String[bin.length()/8];
+        for( int i = 0; i < binArray.length; i++ ) {
+            binArray[i] = "";
+            for( int j=0; j<8; j++ ) {
+                binArray[i] += bin.charAt(8*i + j);
+            }
+        }
+
+        return res;
+    }
+
+    public static char countParity( String bin ) {
+        int count = 0;
+
+        for( int i = 0; i < bin.length(); i++ ) {
+            if( bin.charAt(i) == '1' ) {
+                count++;
+            }
+        }
+
+        if( count % 2 == 0 ) {
+            return '0';
+        } else {
+            return '1';
+        }
+    }
+
 }
---
+++ src/Util.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.HashMap;
+
+public class Util {
+
+    //Fonte: https://stackoverflow.com/questions/8640803/convert-hex-string-to-binary-string
+    public static String hexToBinary(String hex) {
+        int len = hex.length() * 4;
+        String bin = new BigInteger(hex, 16).toString(2);
+        if(bin.length() < len){
+            int diff = len - bin.length();
+            String pad = "";
+            for(int i = 0; i < diff; ++i){
+                pad = pad.concat("0");
+            }
+            bin = pad.concat(bin);
+        }
+        return bin;
+    }
+
+    public static String binaryToHex(String bin) {
+        Integer decimal = Integer.parseInt(bin, 2);
+        return Integer.toString(decimal, 16);
+    }
+
+    public static String intToHex(Integer val) {
+        String hex = Integer.toHexString(val);
+        return hex;
+    }
+
+    public static HashMap<String, String> build8b6tTable() throws IOException {
+        File file = new File("table.txt");
+        HashMap<String, String> table = new HashMap<String, String>();
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        while ((st = br.readLine()) != null) {
+            table.put(st.substring(0, 2) , st.substring(3, st.length()));
+        }
+        return table;
+    }
+
+    public static int countSignalDisparity(String signal) {
+        int positive = 0; int negative = 0;
+        for(int i = 0; i < signal.length(); i++) {
+            if( signal.charAt(i) == '-' ) { 
+                negative++;
+            } else if (signal.charAt(i) == '+') { 
+                positive++;
+            }
+        }
+        return positive - negative;
+    }
+    
+    public static int countBinaryDisparity(String bin) {
+        int positive = 0; int negative = 0;
+        for(int i = 0; i < bin.length(); i ++) {
+            if( bin.charAt(i) == '0' ) { 
+                negative++;
+            } else if (bin.charAt(i) == '1') { 
+                positive ++;
+            }
+        }
+        return positive - negative;
+    }
+
+    public static String invertSignal(String signal) {
+        String res = "";
+        for(int i = 0; i < signal.length(); i ++) {
+            if( signal.charAt(i) == '-' ) { 
+                res += "+";
+            } else if (signal.charAt(i) == '+') { 
+                res += "-";
+            } else {
+                res += signal.charAt(i);
+            }
+        }
+        return res;
+    }
+
+    public static int countZeros(String bin) {
+        int count = 0;
+        for( int i = 0; i < bin.length(); i++ ){
+            if(bin.charAt(i)-'0' == 0){ count++; }
+        }
+        return count;
+    }
+    
+}
